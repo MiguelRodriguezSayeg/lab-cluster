09179ae184b067bd8f62564384a4d77f74183110
MiguelRodriguezSayeg <41605911+MiguelRodriguezSayeg@users.noreply.github.com>
2019-02-22 15:15:01 -0600

    Delete Communication.h

--- Communication.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMMUNICATION_H_
-#define COMMUNICATION_H_
-#include "UARTDriver.h"
-uint_8 COMM_bfnReceiveMsg (uint_8* bpDataRx);
-uint_8 COMM_bfnSendMsg (uint_8* bpMsgToSend, uint_8 bMsgSize);
-void COMM_vfnDriverInit (void);
-#endif /* COMMUNICATION_H_ */
